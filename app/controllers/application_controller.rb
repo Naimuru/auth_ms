82d1c6af8c0680d9255a676167214513db60bdfb
naimuru <nbello@unal.edu.co>
2024-03-11 23:31:29 -0500

    Added aditional parameters in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::API
     include DeviseControllerPatch
     include ActionController::MimeResponds
-end
+  
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  
+    protected
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:emailAddr, :userName, :nickname, :keyIdAuth])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:emailAddr, :userName, :nickname, :keyIdAuth])
+    end
+  end
+  
