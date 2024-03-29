ae6b124cfaa6bbb4015e502266de2dc50e61c3e1
naimuru <nbello@unal.edu.co>
2024-03-29 02:03:16 -0500

    removed unnecessary fields

--- db/schema.rb
+++ db/schema.rb
@@ -1,14 +1,3 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_03_12_042140) do
   # These are extensions that must be enabled in order to support this database
@@ -22,14 +11,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_12_042140) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "userName"
     t.string "nickname"
-    t.string "emailAddr"
     t.string "keyIdAuth"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["emailAddr"], name: "index_users_on_emailAddr", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["userName"], name: "index_users_on_userName", unique: true
   end
 
 end
